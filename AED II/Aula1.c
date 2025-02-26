bb2fbea025ef89460a956970330667270ef39df6
Higor Freitas <silhigor.freitas@gmail.com>
2025-02-25 21:03:12 -0300

    Commit comitted

---
+++ AED II/Aula1.c
@@ -0,0 +1,49 @@
+//Aula de AED II, estruturas em HDs
+//Prof Digiampietri
+
+//Tratamento de  Memória secundaria (HD, SDD etc...)
+
+//Arvores AVL, são todas LOG base 2, ou sejam rápido pra caramba, praticamente instantaneo.
+//Pilha e Fila também tem suas utilizades
+
+//Contudo há muito a mexer.
+
+//Dados complexos
+    //Estrutura de redes socais
+    //Mapas
+    //Grafos, relações
+    //Entre outros
+//Ou seja, nem tudo é inserir ou excluir
+
+
+//Se a quantidade de dados não caber na memoria ram, temos que resolver de outra maneira, que é guaradando no hd
+
+    //HD é possui lentidão ordem de grandezas pior que a memória
+    //Ou seja, precisamos organizar dados em disco e "PROCESSAR" coisas em disco
+    //Coias aprendidas em AED I muitas vezes não se aplicam, pois elas supoem que tudo está carregado.
+
+
+//Grafos
+    //Parte grande da matemática mas que é puxada bastante para computação
+    //DIversas coisas podem ser representadas (Distancia, relação etc)
+    //Vertices e Arestas (Nós e arcos, tanto faz)
+
+//Arquivos e Hashing
+    //Organização de disco, 
+    //Tipos de alocação, Sequencial, Ligada e Indexada
+    //Arvores B e B+
+    //BLocos e informação, unidade minima de leitura é muito maior
+    //Tipos diferentes de Hashing (Tipo de indexação)
+
+
+//Salvar e ler arquivos é importante, então uma IDE é importante
+
+
+//Avaliação
+    //Dois EP e duas provas (tudo individual)
+    //25% Eps e 75% Provas
+    //40% Pi e 60% p2
+
+
+
+
