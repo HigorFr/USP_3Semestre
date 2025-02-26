8101c4a5f927543ccb669ad2dba391e93adfe390
Higor Freitas <silhigor.freitas@gmail.com>
2025-02-26 18:33:29 -0300

    OAC

--- OAC/Aula1.c
+++ OAC/Aula1.c
@@ -1,2 +1,57 @@
-//Prof
+//Prof Clodoaldo
 
+//Bibligrafia AOC WALIIAN STALLINGS 10 Edi
+
+//Disposições Gerais:
+//Estrutura de computadores
+//Interface Sotware-Hardware
+
+//EX Memória Hierarquia:
+    //HD
+    //RAM
+    //Barramento (???)
+    //CACHE (Na MB)
+    //PROCESSADOR
+
+//Cache é um bloco de dados para serem jogados no processador
+
+//Função do OS, no gerenciamento de memória e drivers.
+
+//Pega conteudos da memtematica boelana
+//Circuito logico
+//Instruções no processador
+//Representação
+//Pipeline (EM OAC 2)
+//Assembly (Linguagem de montagem)
+    //Traduzindo C para Assembly
+    //Baixissimo nível
+
+//OAC 1 é mais organização e OAC 2 é mais arquitetura
+
+//DMA
+
+//P1 na terça 13/05 com todo conteudo anterior
+//P2 na --- 01/07  com tudo anterior
+//Sub (03/07)
+
+//70% de presença
+//2 EPs
+// 50% em cada prova em cada Ep
+// 80% Prova e 20% EP (Ou seja, cada Ep vale 1, e cada prova 4)
+
+
+//Arquietura: Referente ao conjunto  de instrução do programa, Entrada e Saídas, Sistema Operacional e periféricos. Tudo vísivel ao programador para programador de baixo nível
+
+//Organização: Detalhes do hardware, conexão entre partes, Instruções, sinais de controle etc...
+
+//Será abordado aspectos Física, Eletromagnetismo etc...
+//Abstração -> "Subir o nível" Basicamente esconder detalhes técnicos e passar só uma interface mais simples para um nível superior
+//Otimizar casos comum (Ou seja, vale a pena tentar chutar casos para tentar melhorar)
+//Pipeline não é paralelismo, é Pseudo-Paralelismo, elas não são simuntâneas de fato mas melhoram desemepnho.
+//Conceito de redundância, Detectar falhas, corrigir erros, backups
+
+//O que faz a ponte entre Hardware e aplicativo é um tipo de Sotware de Sistema
+    //O OS é um exemplo deles
+    //Os drivers também
+    //Compiladores (Aqueles que leem seu código do programa)
+    //
