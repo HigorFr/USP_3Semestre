4ebd63102628cd2c5087e7d0d78eb6950db1d1b8
Higor Freitas <silhigor.freitas@gmail.com>
2025-02-26 20:32:42 -0300

    COO

---
+++ COO/Aula1.java
@@ -0,0 +1,39 @@
+//Computação Orientada a Ojetos
+
+    //Prof Coutinho
+    //flcoutinho@usp.br
+
+    //Nível mais alto que em C, mais abstrato
+    //Orientação à objetos é um dos chamados "Paradigmas da Programação"
+    //Programação em JAVA
+        //Bibliotecas
+    //Organização e arquitetura do código ficam diferentes, mais intuitivas e seguras
+        //Mais fácil de alterar e estruturar em equipes
+
+    //Outro tópicos
+        //Tratar Exceção
+        //Tipo Generio
+        //Coleções (Bibliotecas que implementam estrutura de dados)
+        // Bibliotecas I/O do JAVA
+        //SOLID (Pavimentar caminho para aprender Padrões de Projeto)
+        
+
+    //Observação do prof: essa metéria é mais escalar, não é problemas simples ou diretos, agora é entender como tornar os códigos flexíveis e escaláveis. Ela é a primeira da graduação com foco nisso (diferente de IAA, AED, e IP) e irá pavimentar para outras como Engenharia de Software, Sistemas distribuidos, etc...
+
+
+//O Java
+    //Parecida com C e C++, sem alguns recursos de baixo nível (Tipo ponteiro)
+    //Compila uma vez, roda em todo lugar, idenepdente do sistema
+    //Gerencia memoria (Garbage collector, memória alocada não usada(apontada) é exluída automaticamente)
+    //Variedade de biblioteca
+    //Mais encapsulado que o C
+
+    //No geral essas características se encaixam no C# também
+
+
+
+
+    
+public class Aula1 {
+    
+}
---
+++ COO/OlaMundo.java
@@ -0,0 +1,12 @@
+//E aqui começaremos a mexer em java, deus nos ajude.
+
+
+
+
+//Codigo basico de teste do JAVA
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
+
