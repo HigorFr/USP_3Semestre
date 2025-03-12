84a8ddb0eaba4298ec6c83e0152e2d2909c734aa
Higor Freitas <silhigor.freitas@gmail.com>
2025-03-11 21:14:11 -0300

    OAC

---
+++ OAC/Aula3.c
@@ -0,0 +1,33 @@
+//Convenções para interperação do binário
+    //Exemplo com display numérios
+    //Representações no computador também devem seguir uma estrutura
+
+//Representação númerica já é uma representação (Por padrão a hindu-arabica)
+//Matemticamenticamente usando a base decimal pela quantidade de dedos
+
+//Cada dígito (Cada 1, cada Fio) é chamado de Bit
+    //Em octal precisamos de 3 bits por exemplo para represntar
+    //Em hexadecimal é 4 (Usado muito em eletrónica)
+
+//Transformações em Hexa pra binario é a transformação individual
+//2F8 é 1011111000 - É 10-1111-1000 (Ou seja, cada um individual)
+
+
+//Bit mais à esquerda é o mais representativo
+
+//Em notação, colocamos sempre 
+    //b no final para falar que é binário
+    //0 à esquerda para falar que é ocatal
+    //0x no inicio ou h no final para falar que é hexadecimal
+
+//Não é qualquer representação que é boa para fazer cálculos, por isso usamos essas em específicas
+    //Tipo somar numero romano, que exige que transformemos em decimal primeiro
+
+//Também note que 1 não significa exatamente 1, ele pode significar 1,10,100 pois é uma representação posicional.
+    
+    
+
+
+
+
+
