1188716d88d2d43ce63de49b697d1ff0239989c5
Higor <higor.freitas@usp.br>
2025-06-18 20:09:28 -0300

    Funcional

---
+++ COO/Aula25.java
@@ -0,0 +1,22 @@
+public class Aula25 {
+    
+
+    //Continuando aula de mapas (particularmnete essa é a terceira aula e eu ainda não entendi nada)
+
+    //Mapa é mais simples que muitas outras coleções, mas pode ser facilmente substitido
+        //Por exemplo, guardar quantidade de letras de uma palavra, poderia ser um Map<Character, Integer> onde a chave é a letra e o valor é a quantidade de vezes que ela aparece na palavra
+
+        //Entrada e saída em java.
+
+
+        //Principalemnete gerenciando pela entrada STD IO do java.
+            //Fluxos de dados em geral, tudo com relação ao arquivo ou à saida do terminal.
+            //O proprio printi do System.out.println() é um fluxo de dados, que escreve na saída padrão do sistema, que é o terminal.
+            //Vamos estudar a arvore  de classes que definem essas funções padrões do java
+
+            
+
+
+
+
+}
