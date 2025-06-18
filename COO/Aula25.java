decea6c8b4a798d01e80823ba3da220461fc9666
Higor <higor.freitas@usp.br>
2025-06-18 20:40:49 -0300

    COO

--- COO/Aula25.java
+++ COO/Aula25.java
@@ -20,7 +20,18 @@ public class Aula25 {
             //Elas são classes abstratas, ou seja, regem o compartamento mais básico de entradas e saídas
 
 
-        //
+        //BufferedReader e BufferedWriter é para ler e escrever dados de forma eficiente, usando buffers para melhorar o desempenho
+            //Ele acumula uma fila de alterações de arquivos, para jogar tudo de uma vez, que é amis eficiente do que ficar chamando a memória toda hora.
+
+        //Zip é para compactar o numero de bytes que está sendo escrito
+
+        //Note que essa calsses estendem a classe Reader e Writer, que são classes abstratas que definem os métodos básicos para ler e escrever caracteres
+            //Contudo elas não especificam de fato, não é uma extensão direta, é como se fosse algum outro tipo de classe completamente, ela acrescenta pouco. As vezes é só para agrega-las em um mesmo lugar.
+
+        //Se eu precisar de várias dessas coisas simultanemanete, tipo algo em buffer e compacto (Buffer e Zip) você vai simplesmente encadear as classes (Como se fosse um composição). Desse modo o buffer receberia algo que já passou le afunção ZIP. É como um decorar (Cascas em torno de um objeto)
+
+
+        
         
 
 
